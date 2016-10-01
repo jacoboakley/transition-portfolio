762e7663a84fa080f4ec1a29c666954e0cc2c4f1
jacoboakley <jacoboakley21@gmail.com>
2016-10-01 03:55:52 -0400

    updated HTML

--- assets/my-scripts.js
+++ assets/my-scripts.js
@@ -1,20 +1,25 @@
 var welcome = document.getElementById("home");
-welcome.style.transition = "opacity 3s";
+welcome.style.transition = "opacity 4s";
 
 var mainPage = document.getElementById("main");
-mainPage.style.transition = "opacity 3s";
+mainPage.style.transition = "opacity 4s";
+
+var hText = document.getElementById("homeText");
+hText.style.transition = "opacity 4s";
 
 function fade() {
-  welcome.style.opacity = 0;
+  welcome.style.opacity = 0.5;
   welcome.style.zIndex = -1;
   mainPage.style.display = "initial";
   mainPage.style.opacity = 1;
+  hText.style.opacity = 0;
 }
 
 function unfade () {
   welcome.style.opacity = 1;
   welcome.style.zIndex = 1;
   page1.style.opacity = 0;
+  hText.style.display = "initial";
 }
 
 var hButton = document.getElementById("homeButton");
